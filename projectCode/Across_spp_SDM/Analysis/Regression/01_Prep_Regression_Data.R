ba0820d6c5208957e70a469ac8183764ee6bbc00
Liz Amador <lizbeth.amador@maine.edu>
2025-11-10 12:53:14 -0500

    main regression scripts

--- projectCode/Across_spp_SDM/Analysis/Regression/01_Prep_Regression_Data.R
+++ projectCode/Across_spp_SDM/Analysis/Regression/01_Prep_Regression_Data.R
@@ -1,28 +1,25 @@
-#Prep Data for Regression 
-#Author: Lizbeth G Amador 
+#Prep Data for Regression
+#Author: Lizbeth G Amador
 
-#This script merges the ensemble projection differences data for phenology and 
-#range. Descriptive information is added and outliers are removed. 
+#This script merges the ensemble projection differences data for phenology and
+#range. Descriptive information is added and outliers are removed.
 
 
 ############
 # Presets
 ############
-if (!requireNamespace("tidyverse", quietly = TRUE)) {
-  install.packages("tidyverse")}; library(tidyverse)
-
-if (!requireNamespace("terra", quietly = TRUE)) {
-  install.packages("terra")}; library(terra)
-
-if (!requireNamespace("dggridR", quietly = TRUE)) {
-  install.packages("dggridR")}; library(dggridR)
+suppressPackageStartupMessages({
+  if (!requireNamespace("tidyverse", quietly = TRUE)) {install.packages("tidyverse")}; library(tidyverse)
+  if (!requireNamespace("terra", quietly = TRUE)) { install.packages("terra")}; library(terra)
+  if (!requireNamespace("dggridR", quietly = TRUE)) {install.packages("dggridR")}; library(dggridR)
+})
 
 ##############
 # Directories
 #############
-main = "/users/PUOM0017/lamad/esiil_macrophenology"
-L2 = file.path(main, "Data/L2")
-graphs = file.path(main, "analysis/4-regression/Graphs")
+mwd = "~/data-store/home/lamador/Data"
+L2 = file.path(mwd, "L2")
+graphs = file.path(L2, "Graphs")
 
 #########
 # Data
@@ -31,12 +28,12 @@ graphs = file.path(main, "analysis/4-regression/Graphs")
 rang = read.csv(file.path(L2, "AllSpecies_EMProj_diff_Range.csv"), header = TRUE)
 phe = read.csv(file.path(L2, "AllSpecies_EMProj_diff_Phenology.csv"), header = TRUE)
 
-#Clean up the data 
+#Clean up the data
 setdiff(unique(rang$species), unique(phe$species))
-#save the species in common 
+#save the species in common
 comm.sp = intersect(unique(rang$species), unique(phe$species))
 
-#filter for common species overlap 
+#filter for common species overlap
 rang.comm.sp = rang %>%
   filter(species %in% comm.sp) %>%
   rename(rdiff = diff_val) %>%
@@ -50,62 +47,47 @@ rm(phe, rang); gc()
 
 #Merge the two (whichiver one ihas more rows will be first)
 all.data = left_join(rang.comm.sp, phe.comm.sp, by = c("latitude", "longitude", "species"))
+all.data = all.data[!is.na(all.data$rdiff),]
 #free up space
 rm(rang.comm.sp, phe.comm.sp); gc()
-#Add more species information 
+
+################
+# Species info
+###############
+#Add more species information
 sp.info = read.csv(file.path(L2, "Focal_Species_List_Information.csv"), header = TRUE)
-#phenology list filter by presence threshold only 
-all.data = left_join(all.data, sp.info, by = "species") 
+#phenology list filter by presence threshold only
+all.data = left_join(all.data, sp.info, by = "species")
 
-#Add NEON Domain information 
-domains = vect(file.path(L2, "conus.shp"))
-v = vect(all.data, geom = c("longitude", "latitude"), keep = TRUE, crs <- crs(domains))
-#free up space
-rm(all.data); gc()
-#intersect to get domain information 
-v.domain = intersect(v, domains)
-#Free up space
-rm(v, v.domain); gc()
-#turn back into data frame 
-all.data.d = as.data.frame(v.domain)
-# names(all.data.d)
-#remove columns
-all.data.d = all.data.d %>%
-  select(-c("OBJECTID", "Shape_Leng", "DomainName", "Shape_Le_1", "Shape_Area")) %>%
-  rename(domain_id = DomainID)
-
-
-#Save summary info 
+#Save summary info
 sink(file= file.path(graphs, "All_Species_Info_Tables_Range_Phenology.txt"))
-a = all.data.d %>% count(status, functional_type)
+a = all.data %>% count(status, functional_type)
 print(a)
 
-b = all.data.d %>% 
-  group_by(species, functional_type, status, domain_id) %>% 
+b = all.data %>%
+  group_by(species, functional_type, status) %>%
   summarise(n())
 print(b, n = 1654)
 
-table(all.data.d$status)
-table(all.data.d$functional_type)
-table(all.data.d$genus_common_name)
-table(all.data.d$species)
+table(all.data$status)
+table(all.data$functional_type)
+table(all.data$genus_common_name)
+table(all.data$species)
 
-unique(all.data.d$species)
-unique(all.data.d$domain_id)
+unique(all.data$species)
 sink()
 
-
 #save object
-save(all.data.d, file = file.path(L2,"All_Species_Data_Range_Phenology.RData"))
+# save(all.data, file = file.path(L2,"All_Species_Data_Range_Phenology.RData"))
 # load(file = "~/esiil_macrophenology/Data/L2/All_Species_Data_Range_Phenology.RData", verbose = TRUE)
 # load(file = "All_Species_Data_Range_Phenology.RData", verbose = TRUE)
 
 
 #########################
-# Regression Assumptions  
+# Regression Assumptions
 ##########################
-#Remove outliers by species 
-model.data.clean <- all.data.d %>%
+#Remove outliers by species
+model.data.clean <- all.data %>%
   filter(!is.na(pdiff)) %>%
   group_by(species) %>%
   mutate(
@@ -121,7 +103,7 @@ model.data.clean <- all.data.d %>%
   ungroup() %>%
   select(-Q1, -Q3, -IQR, -lower, -upper)  #clean up helper columns
 
-#remove outliers by status 
+#remove outliers by status
 model.data.no_outliers <- model.data.clean %>%
   group_by(status) %>%
   mutate(
@@ -136,8 +118,8 @@ model.data.no_outliers <- model.data.clean %>%
   select(-Q1, -Q3, -IQR, -lower_bound, -upper_bound)  # optional cleanup
 
 # boxplot(model.data.no_outliers$pdiff ~ model.data.no_outliers$status)
-# 
-# #remove outliers by functional type 
+#
+# #remove outliers by functional type
 model.data.no_outliers1 <- model.data.no_outliers %>%
   group_by(functional_type) %>%
   mutate(
@@ -150,14 +132,14 @@ model.data.no_outliers1 <- model.data.no_outliers %>%
   filter(pdiff >= lower_bound & pdiff <= upper_bound) %>%
   ungroup() %>%
   select(-Q1, -Q3, -IQR, -lower_bound, -upper_bound)  # optional cleanup
- 
+
 # pdf(file.path(graphs, "All_Data_ModelData_NoOut_Plots.pdf"))
 # boxplot(model.data.no_outliers1$pdiff ~ model.data.no_outliers1$functional_type)
 # boxplot(model.data.no_outliers1$pdiff ~ model.data.no_outliers1$status)
 # hist(model.data.no_outliers1$pdiff)
 # hist(model.data.no_outliers1$rdiff)
 # hist(model.data.no_outliers1$latitude)
-# 
+#
 # hist(log(model.data.no_outliers1$latitude))
 # hist((model.data.no_outliers1$latitude)^2)
 # hist((model.data.no_outliers1$latitude)^3)
@@ -165,24 +147,49 @@ model.data.no_outliers1 <- model.data.no_outliers %>%
 #
 model.data.no_outliers1$lat.sq = model.data.no_outliers1$latitude^2
 model.data = model.data.no_outliers1
-save(model.data, file=file.path(L2, "All_Data_ModelData_NoOutliers.RData"))
+# save(model.data, file=file.path(L2, "All_Data_ModelData_NoOutliers.RData"))
 # load(file = file.path(L2, "All_Data_ModelData_NoOutliers.RData"), verbose = TRUE)
+
+
+###############
+# NEON Domains
+###############
+#Add NEON Domain information
+domains = vect(file.path(L2, "conus.shp"))
+v = vect(model.data, geom = c("longitude", "latitude"), keep = TRUE, crs <- crs(domains))
+#free up space
+rm(all.data); gc()
+#intersect to get domain information
+v.domain = intersect(v, domains)
+#Free up space
+rm(v, v.domain); gc()
+#turn back into data frame
+model.data.d = as.data.frame(v.domain)
+# names(model.data.d)
+#remove columns
+model.data.d = model.data.d %>%
+  select(-c("OBJECTID", "Shape_Leng", "DomainName", "Shape_Le_1", "Shape_Area")) %>%
+  rename(domain_id = DomainID)
+
+save(model.data.d, file = file.path(L2, "ModelData_NoOutliers.RData"))
+# load(file = file.path(L2, "ModelData_NoOutliers.RData"), verbose = TRUE)
 
 
 #################
 #Subsampling
 #################
-#For computational ease we are going to take a sample from our main dataset. 
+#For computational ease we are going to take a sample from our main dataset.
 #We will be using the best practices workflow from the eBird best practices workshop
-#https://strimas.com/ebp-workshop/subsampling.html 
-#https://cran.r-project.org/web/packages/dggridR/vignettes/dggridR.html 
+#https://strimas.com/ebp-workshop/subsampling.html
+#https://cran.r-project.org/web/packages/dggridR/vignettes/dggridR.html
 
-#randomly subset data 
-#generate hexagonal grid 
-dggs <- dgconstruct(res = 10)
+#randomly subset data
+r = 11
+#generate hexagonal grid
+dggs <- dgconstruct(res = r)
 #> Resolution: 13, Area (km^2): 31.9926151554038, Spacing (km): 5.58632116604266, CLS (km): 6.38233997895802
 #read in data
-mod.data.cell <- model.data %>%
+mod.data.cell <- model.data.d %>%
   #get hexagonal cell id and week number for each checklist
   mutate(cell = dgGEO_to_SEQNUM(dggs, longitude, latitude)$seqnum)
 #sample from each instance
@@ -193,47 +200,84 @@ sub.data <- mod.data.cell %>%
   ungroup()
 #subset data dimensions
 dim(sub.data)
-save(sub.data, file=file.path(L2, "Subset_Data_ModelData_NoOutliers.RData"))
-# load(file = file.path(L2, "Subset_Data_ModelData_NoOutliers.RData"), verbose = TRUE)
+save(sub.data, file=file.path(L2, "Subset_ModelData_NoOutliers.RData"))
+# load(file = file.path(L2, "Subset_ModelData_NoOutliers.RData"), verbose = TRUE)
+
+
+#Save summary info
+sink(file= file.path(L2, "All_Species_Info_Tables_Range_Phenology_ModelData_Subset.txt"))
+print("resolution: ", r)
+str(sub.data)
+
+cat("\n range of psd\n")
+range(sub.data$psd)
+cat("\n range of psd\n")
+range(sub.data$rsd)
+
+unique(sub.data$species)
+unique(sub.data$domain_id)
+unique(sub.data$dispersal)
+unique(sub.data$functional_type)
+
+a = sub.data %>% count(nativity, funct_type, dispersal)
+print(a)
+
+b = sub.data %>%
+  group_by(species, nativity, funct_type, dispersal, domain_id) %>%
+  summarise(n())
+print(b, n = length(b))
+
+table(sub.data$nativity)
+table(sub.data$funct_type)
+table(sub.data$genus_common_name)
+table(sub.data$species)
+
+cat("\number ofspecies\n")
+c = sub.data %>%
+  group_by(species, nativity, funct_type, dispersal) %>%
+  summarise(n())
+table(c$nativity)
+table(c$funct_type)
+table(c$dispersal)
+c1 = c %>%
+  group_by(nativity, funct_type, dispersal) %>%
+  summarise(n())
+print(c1)
+
+sink()
+
 
 
 #################
-#Adjacency table 
+#Adjacency table
 #################
-domains = vect(file.path(L2, "conus.shp"))
-#Build adjacency table for the NEON domains 
-#Load domain polygons (assuming already read into 'domains')
-domains <- makeValid(domains)
-#Merge (dissolve) polygons by DomainID -- This ensures one polygon per domain
-# domains <- aggregate(domains, by = "DomainID", fun = first)
-domains <- aggregate(domains, by = "DomainID", fun = mean, na.rm = TRUE)
-#Determine touching relationships (adjacency)
-adj_mat <- relate(domains, domains, relation = "touches")
-#Convert logical matrix to binary matrix (0/1)
-adj_binary <- as.matrix(adj_mat) * 1
-#Use DomainID as row/column names (now unique)
-rownames(adj_binary) <- colnames(adj_binary) <- domains$DomainID
-#Manually enter touch boundary for some missing ones
-#List of domain ID pairs to force as neighbors
-adj_pairs <- list(
-  c("2", "3"), c("3", "11"), c("6", "9"), c("6", "10"), c("8", "10"), c("9", "5"),
-  c("9", "6"), c("10", "6"), c("10", "8"), c("10", "11"), c("11", "3"), c("11", "10"),
-  c("11", "13"), c("16", "15"), c("16", "17")
-)
-#Add symmetric adjacency (both directions)
-for (pair in adj_pairs) {
-  i <- pair[1]
-  j <- pair[2]
-  adj_binary[i, j] <- 1
-  adj_binary[j, i] <- 1
-}
-#Save to CSV
-write.csv(adj_binary, file.path(L2, "neon_adjacency_matrix.csv"), row.names = TRUE)
-
-
-
-
-
-
-
+# domains = vect(file.path(L2, "conus.shp"))
+# #Build adjacency table for the NEON domains
+# #Load domain polygons (assuming already read into 'domains')
+# domains <- makeValid(domains)
+# #Merge (dissolve) polygons by DomainID -- This ensures one polygon per domain
+# # domains <- aggregate(domains, by = "DomainID", fun = first)
+# domains <- aggregate(domains, by = "DomainID", fun = mean, na.rm = TRUE)
+# #Determine touching relationships (adjacency)
+# adj_mat <- relate(domains, domains, relation = "touches")
+# #Convert logical matrix to binary matrix (0/1)
+# adj_binary <- as.matrix(adj_mat) * 1
+# #Use DomainID as row/column names (now unique)
+# rownames(adj_binary) <- colnames(adj_binary) <- domains$DomainID
+# #Manually enter touch boundary for some missing ones
+# #List of domain ID pairs to force as neighbors
+# adj_pairs <- list(
+#   c("2", "3"), c("3", "11"), c("6", "9"), c("6", "10"), c("8", "10"), c("9", "5"),
+#   c("9", "6"), c("10", "6"), c("10", "8"), c("10", "11"), c("11", "3"), c("11", "10"),
+#   c("11", "13"), c("16", "15"), c("16", "17")
+# )
+# #Add symmetric adjacency (both directions)
+# for (pair in adj_pairs) {
+#   i <- pair[1]
+#   j <- pair[2]
+#   adj_binary[i, j] <- 1
+#   adj_binary[j, i] <- 1
+# }
+# #Save to CSV
+# write.csv(adj_binary, file.path(L2, "neon_adjacency_matrix.csv"), row.names = TRUE)
 
---
+++ projectCode/Across_spp_SDM/Analysis/Regression/03_brms_uncertainty.R
@@ -0,0 +1,234 @@
+## BRMS linear regression with uncertainty
+## Authors: Tong Qiu, Lizbeth G Amador
+
+
+#Libraries
+suppressPackageStartupMessages({
+  if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse"); library(tidyverse)
+  if (!requireNamespace("brms", quietly = TRUE)) install.packages("brms"); library(brms)
+  if (!requireNamespace("caret", quietly = TRUE)) install.packages("caret"); library(caret)
+})
+
+#########
+run = 1
+#########
+#######
+# Data
+#######
+#Working directories
+mwd = "~/data-store/home/lamador/Data"
+L2 = file.path(mwd, "L2")
+graphs = file.path(L2, "Graphs")
+
+#Model data
+load(file = file.path(L2, "Subset_ModelData_NoOutliers.RData"), verbose = TRUE) # variables of importance: pdiff, psd, rdiff, rsd, domain_id, funct_tpe, nativity, dispersal
+dim(sub.data)
+#NEON Domain adjaceny table -- Important to read as a matrix!
+dom.mat <- as.matrix(read.csv(file.path(L2, "neon_adjacency_matrix.csv"),header = TRUE, row.names = 1, check.names = FALSE))
+#Making sure it's numeric
+mode(dom.mat) <- "numeric"
+
+
+######################################
+# Model calibration/validation groups
+######################################
+message("1) Create calibration and validation groups")
+#Dummy data for testing code [remove slicing for actual runs]
+sub.data <- sub.data %>%
+  slice_sample(n = 1000)   # keep exactly n rows
+#Training & testing groups -- for model validation
+set.seed(run)   # Set a seed value for reproducability purposes in this document
+#Randomly select 70% of the data (rows)
+index <- sample(1:nrow(sub.data), size=floor(.70*nrow(sub.data)))
+#Training group (initial model)
+train.dat <- sub.data[index, ]
+#Testing group
+test.dat  <- sub.data[-index, ]
+dim(train.dat); dim(test.dat)
+#save space
+rm(sub.data)
+#Training data without woody species (for second model)
+train.dat1 = train.dat[train.dat$funct_type == "non-woody", ]
+test.dat1 = test.dat[test.dat$funct_type == "non-woody", ]
+
+
+#####################
+# Linear assumptions
+#####################
+#message("Exporting linear assumptions")
+#Model with functional type (No nativity)
+#pdf(file = file.path(L2, "Graphs", paste0("brms_fit_LinearAssumptions_Run", run, "_funct_type.pdf")))
+#mod <- lm(pdiff ~ rdiff + latitude + funct_type + dispersal + rdiff*latitude, data = train.dat)
+#1. Linearity & Homoscedasticity
+#par(mfrow = c(2,2))
+#plot(mod)
+#2. Normality of residuals
+#hist(resid(mod), breaks = 100, main = "Histogram of Residuals", xlab = "Residuals")
+#3. Q-Q plots
+#qqnorm(resid(mod)); qqline(resid(mod), col = "red")
+#dev.off()
+
+#Model with nativity (No functional type)
+pdf(file = file.path(L2, "Graphs", paste0("brms_fit_LinearAssumptions_Run", run, "_nativity.pdf")))
+mod <- lm(pdiff ~ rdiff + latitude + nativity + dispersal + rdiff*latitude, data = train.dat1)
+#1. Linearity & Homoscedasticity
+par(mfrow = c(2,2))
+plot(mod)
+#2. Normality of residuals
+hist(resid(mod), breaks = 100, main = "Histogram of Residuals", xlab = "Residuals")
+#3. Q-Q plot
+qqnorm(resid(mod)); qqline(resid(mod), col = "red")
+dev.off()
+#save space
+rm(mod); gc()
+
+
+x.time = system.time({ #START timer
+  ###################
+  ## Fit BRMS model:
+  ###################
+  ##   - Gaussian identity
+  ##   - Response measurement error via  | se(phen_sd (pdiff), sigma = TRUE)
+  ##   - Predictor measurement error via me(range_mean (rdiff), range_sd (rsd))
+  ##   - Random intercept by species (can easily change to site or other factors)
+  # message("2) Begin first brms model -- All woody & non-woody, w/o nativity")
+  # #----------- All woody & non-woody (w/o nativity status) -----------
+  # #Not including nativity status since only non-woody functional types are invasive
+  # #Formua
+  # form <- bf(
+  #   pdiff | se(psd, sigma = TRUE) ~
+  #     me(rdiff, rsd) + latitude + funct_type + dispersal + rdiff*latitude +
+  #     (1 | species) + #random effect
+  #     car(dom.mat, gr = domain_id)) #adjacency matrix
+  # message(form)
+  # #Setting priors
+  # priors <- c(
+  #   set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
+  #   set_prior("normal(0, 1)",         class = "b"),      # includes slope for me()
+  #   set_prior("exponential(1)",       class = "sd"),     # RE SD
+  #   set_prior("student_t(3, 0, 2.5)", class = "sigma")   # residual SD (beyond phen_sd)
+  # )
+  # #Fitting model
+  # fit <- brm(
+  #   formula = form,
+  #   data    = train.dat,
+  #   data2 = list(dom.mat=dom.mat), # adjacency matrix data
+  #   family  = gaussian(),           # identity link on [-1, 1]
+  #   prior   = priors,
+  #   chains  = 4, iter = 3000, warmup = 1000, cores = 4,
+  #   #threads = threading(16),
+  #   seed    = 20250909,
+  #   backend = "cmdstanr", #to avoid TBB error in Cyverse HPC environment
+  #   control = list(adapt_delta = 0.95, max_treedepth = 13))
+  # message("Modeling complete! ... Saving model fit")
+  # saveRDS(fit, file = file.path(L2, paste0("brms_fit_training_Run", run, "_funct_type.rds")))
+  # message("Predicting using the testing dataset")
+  # #predict testing data using the training model
+  # fit.t <- predict(fit, newdata = test.dat)
+  # saveRDS(fit.t, file = file.path(L2, paste0("brms_fit_testing_Run", run, "_funct_type.rds")))
+  #
+  #
+  # #--- save output ---
+  # message("saving output")
+  # sink(file.path(graphs, paste0("brms_fit_Run", run, "_funct_type_output.txt")))
+  # cat("Training\n"); cat("Summary:\n")
+  # print(summary(fit))
+  # cat("\nPrior summary:\n")
+  # print(prior_summary(fit))
+  # cat("\nLook for the coefficient named b_me... — that's the slope for the latent RANGE.\n")
+  # print(fit)
+  # cat("\nTesting\n"); cat("Summary:\n")
+  # print(summary(fit.t))
+  # print(fit.t)
+  # #Posterior Mean predictions
+  # #Posterior expected value per observation
+  # train_fitted <- fitted(fit)  # matrix with Estimate, Est.Error, Q2.5, Q97.5
+  # # Posterior mean predictions
+  # train_mean <- train_fitted[, "Estimate"]; test_mean <- fit.t[, "Estimate"]  # if predict() returns a matrix with Estimate
+  # cat("\nRMSE\n")
+  # rmse_train <- sqrt(mean((train.dat$pdiff - train_mean)^2)); rmse_test <- sqrt(mean((test.dat$pdiff - test_mean)^2))
+  # cat("training: ", round(rmse_train, 4)); cat("\ntesting: ", round(rmse_test, 4))
+  # cat("\nR^2\n")
+  # r2_train <- cor(train.dat$pdiff, train_mean)^2; r2_test  <- cor(test.dat$pdiff, test_mean)^2
+  # cat("training: ", round(r2_train, 4)); cat("\ntesting: ", round(r2_test, 4))
+  # sink()
+  #-------------------
+
+
+  #----------- All non-woody (w/ nativity status) -----------
+  message("3) Begin second brms model -- All non-woody, w/ nativity")
+  #Removing woody species (and therefor function type) from the model (only non-woody invasive spp.)
+  #Formula
+  form <- bf(
+    pdiff | se(psd, sigma = TRUE) ~
+      me(rdiff, rsd) + latitude + nativity + dispersal + rdiff*latitude +
+      (1 | species) + #random effect
+      car(dom.mat, gr = domain_id)) #adjacency matrix
+  message(form)
+  #Setting priors
+  priors <- c(
+    set_prior("student_t(3, 0, 2.5)", class = "Intercept"),
+    set_prior("normal(0, 1)",         class = "b"),      # includes slope for me()
+    set_prior("exponential(1)",       class = "sd"),     # RE SD
+    set_prior("student_t(3, 0, 2.5)", class = "sigma")   # residual SD (beyond phen_sd)
+  )
+  #Fitt model
+  fit <- brm(
+    formula = form,
+    data    = train.dat1,
+    data2 = list(dom.mat=dom.mat), # adjacency matrix data
+    family  = gaussian(),           # identity link on [-1, 1]
+    prior   = priors,
+    chains  = 4, iter = 3000, warmup = 1000, cores = 4,
+    #threads = threading(16),
+    seed    = 20250909,
+    control = list(adapt_delta = 0.95, max_treedepth = 13))
+  message(cat("\tModeling complete! ... Saving model fit"))
+  saveRDS(file = file.path(L2, paste0("brms_fit_training_Run", run, "_nativity.rds")))
+  message(cat("\tPredicting using the testing dataset"))
+  #predict testing data using the training model
+  fit.t <- predict(fit, newdata = test.dat1)
+  saveRDS(fit.t, file = file.path(L2, paste0("brms_fit_testing_Run", run, "_nativity.rds")))
+
+
+  #--- save output ---
+  message(cat("\tsaving output"))
+  sink(file.path(graphs, paste0("brms_fit_Run", run, "_nativity_output.txt")))
+  cat("Training\n")
+  cat("Summary:\n"); print(summary(fit))
+  cat("\nPrior summary:\n"); print(prior_summary(fit))
+  cat("\nLook for the coefficient named b_me... — that's the slope for the latent RANGE.\n")
+  print(fit)
+  cat("\nTesting\n")
+  cat("Summary:\n"); print(summary(fit.t))
+  print(fit.t)
+  #Posterior Mean predictions
+  #Posterior expected value per observation
+  train_fitted <- fitted(fit)  # matrix with Estimate, Est.Error, Q2.5, Q97.5
+  # Posterior mean predictions
+  train_mean <- train_fitted[, "Estimate"]; test_mean <- fit.t[, "Estimate"]  # if predict() returns a matrix with Estimate
+  cat("\nRMSE\n")
+  rmse_train <- sqrt(mean((train.dat1$pdiff - train_mean)^2)); rmse_test <- sqrt(mean((test.dat1$pdiff - test_mean)^2))
+  cat("training: ", round(rmse_train, 4)); cat("\ntesting: ", round(rmse_test, 4))
+  cat("\nR^2\n")
+  r2_train <- cor(train.dat1$pdiff, train_mean)^2; r2_test  <- cor(test.dat1$pdiff, test_mean)^2
+  cat("training: ", round(r2_train, 4)); cat("\ntesting: ", round(r2_test, 4))
+  sink()
+  #-------------------
+}) #END timer
+
+
+sink(file = file.path(graphs, paste0("brms_fit_Run", run, "_nativity_TotalRunTime.txt")))
+min = round(x.time[3]/60)
+if(min > 59){
+  hr = round(min/60)
+  message("Total run time: ", hr, " hours")
+  if(hr > 23){
+    day = round(hr/24)
+    message("Total run time: ", day, " days")
+  }
+} else{
+  message("Total run time: ", min, " minutes")
+}
+sink()
+
